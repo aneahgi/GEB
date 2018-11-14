dd5be10e513e73301eca4cfb61127d678f8bd5f3
Angi <angela.keller@students.unibe.ch>
2018-11-15 00:36:55 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+Todo
+- Logo
+- Beschreibung Zimmer (Säschu)
+- Adresse Hotel (Angi)
+- Startseite Bilder und einführender Text (Livia)
+- geordnete Liste mit vorzüge Hotel (Säschu)
+- keine Inline Styles (Angi)
+ */
